8d093671897e9aba3657b1636ab00242d3bdceef
yogita <veryogita@gmail.com>
2018-09-30 20:44:34 +0530

    Six more projects added

--- js/gallery.js
+++ js/gallery.js
@@ -5,8 +5,8 @@ var galleryItems = [
     imgAlt: 'Image of Employee Directory',
     heading: 'Employee Directory',
     introduction: 'I used a random user API to generate random users and generate their details .Clicking a user would pop up a modal with addditional information. This project included Ajax calls using jQuery ,parsing and formating JSON with javascript and CSS. ',
-    website: 'https://yog9.github.io/Interactive-form--website/',
-    gitLink: 'https://github.com/Yog9'
+    website: 'https://yog9.github.io/Employee-Directory/',
+    gitLink: 'https://github.com/Yog9/Employee-Directory'
   },
   {
     id: 1,
@@ -14,8 +14,8 @@ var galleryItems = [
     imgAlt: 'Image of the Wheel of Success project',
     heading: 'Wheel of Success.',
     introduction: 'Wheel of Success ,is  a word guessing game where players click letters onscreen to try and guess one of five random phrases',
-    website: 'https://yog9.github.io/Interactive-form--website/',
-    gitLink: 'https://github.com/Yog9'
+    website: 'https://yog9.github.io/game-app-show/',
+    gitLink: 'https://github.com/Yog9/game-app-show'
   },
   {
     id: 2,
@@ -24,7 +24,7 @@ var galleryItems = [
     heading: 'Interactive form project',
     introduction: 'For this project, I used my skills to enhance a form so that it’s engaging, interactive, and easy to use. I also made the form more user-friendly by providing helpful error messages when the user enters invalid information into the form fields.',
     website: 'https://yog9.github.io/Interactive-form--website/',
-    gitLink: 'https://github.com/Yog9'
+    gitLink: 'https://github.com/Yog9/Interactive-form--website'
   },
   {
     id: 3,
@@ -32,8 +32,8 @@ var galleryItems = [
     imgAlt: 'Image of the Photo Gallery project',
     heading: 'Photo Gallery',
     introduction: 'This photo gallery includes a search bar so that photos are shown or hidden depending on user input. It allows you to filter images by using keyword searches, such as "snow" or "lake" .The gallery includes a lightbox with navigation for moving to the next/previous photo.',
-    website: 'https://yog9.github.io/Interactive-form--website/',
-    gitLink: 'https://github.com/Yog9'
+    website: 'https://yog9.github.io/Photo-Gallery/',
+    gitLink: 'https://github.com/Yog9/Photo-Gallery'
   },
   {
     id: 4,
@@ -41,8 +41,8 @@ var galleryItems = [
     imgAlt: 'Image of the Responsive Website project',
     heading: 'Responsive Website',
     introduction:'This website was built mobile-first and uses CSS Grid Layouts. ',
-    website: 'https://yog9.github.io/Interactive-form--website/',
-    gitLink: 'https://github.com/Yog9'
+    website: 'https://yog9.github.io/Responsive-Layout/',
+    gitLink: 'https://github.com/Yog9/Responsive-Layout'
   },
   {
     id: 5,
@@ -52,6 +52,60 @@ var galleryItems = [
     introduction: 'In this project, I built a mobile first, responsive registration form using a variety of HTML form input types and attributes. The form was enhanced using HTML5 validation properties to make sure required fields are filled out.Utilizing CSS property transition and pseudo-states,I added effects for highlighting the look of the form fields when they are focused on .',
     website: 'https://yog9.github.io/Registration-Form/',
     gitLink: 'https://github.com/Yog9/Registration-Form'
+  },
+  {
+    id: 6,
+    imgSrc: 'images/svg.png',
+    imgAlt: 'Image of the Svg Animation project',
+    heading: 'SVG ANIMATION',
+    introduction: 'In this project Learned how to modify styles on SVG and animate them. ',
+    website: 'https://yog9.github.io/SVG-ANIMATIONS/',
+    gitLink: 'https://github.com/Yog9/SVG-ANIMATIONS'
+  },
+  {
+    id: 7,
+    imgSrc: 'images/dashboard.png',
+    imgAlt: 'Image of the Web App Dashboard  project',
+    heading: 'Web App Dashboard',
+    introduction: 'In this project,I used Chart.js to create line, bar, and pie charts and also  used local storage to save user changes. Created modals for alert messages and notifications.',
+    website: 'https://yog9.github.io/Web-App-Dashboard/',
+    gitLink: 'https://github.com/Yog9/Web-App-Dashboard'
+  },
+  {
+    id: 8,
+    imgSrc: 'images/styleguide.png',
+    imgAlt: 'Image of the Web Style Guide project',
+    heading:'Web Style Guide',
+    introduction: 'I Learned the fundamentals of Sass. I created a style guide from the ground up using variables, mixins, and functions.',
+    website: 'https://yog9.github.io/Web-Style-Guide/',
+    gitLink: 'https://github.com/Yog9/Web-Style-Guide'
+  },
+  {
+    id: 9,
+    imgSrc: 'images/quote.png',
+    imgAlt: 'Image of the Quote Generator project',
+    heading:'Quote Generator',
+    introduction: 'In this project, I built a mobile first, responsive registration form using a variety of HTML form input types and attributes. The form was enhanced using HTML5 validation properties to make sure required fields are filled out.Utilizing CSS property transition and pseudo-states,I added effects for highlighting the look of the form fields when they are focused on .',
+    website: 'https://yog9.github.io/Quote-Generator/',
+    gitLink: 'https://github.com/Yog9/Quote-Generator'
+  },
+  {
+    id: 10,
+    imgSrc: 'images/videoplayer.png',
+    imgAlt: 'Image of the Interactive Video Player project',
+    heading:'Interactive Video Player',
+    introduction: 'In this project, I built a  custom video player with custom video controls and used JavaScript to highlight the captions from the video as it was playing. Also, if the user selects a section of text, it will autoplay from the selection.',
+    website: 'https://yog9.github.io/Interactive-video-player/',
+    gitLink: 'https://github.com/Yog9/Interactive-video-player'
+  },
+  {
+    id: 11,
+    imgSrc: 'images/profilepage.png',
+    imgAlt: 'Image of the Profile Page project',
+    heading:'Profile Page',
+    introduction: 'In this project, I built a  custom video player with custom video controls and used JavaScript to highlight the captions from the video as it was playing. Also, if the user selects a section of text, it will autoplay from the selection.',
+    website: 'https://yog9.github.io/Profile-Page/',
+    gitLink: 'https://github.com/Yog9/Profile-Page'
   }
 
   ];
