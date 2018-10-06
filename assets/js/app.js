ba9fb79917f5cdf840290d8862df3b375641a123
yogita <veryogita@gmail.com>
2018-10-06 10:30:10 +0530

    Moved files to assets folder,removed unused links ,added smooth scroll functionality and added favicon

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
- new WOW().init();
+ 
 particlesJS.load('particles-js', 'js/particles.json', function() {
    console.log('callback - particles.js config loaded');
  });
@@ -50,7 +50,27 @@ var modal= document.querySelector(".modal1");
   function hasClass(elem, className) {
     return elem.classList.contains(className);
 }
+//smooth scrolling
+// Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
+      // Store hash
+      var hash = this.hash;
 
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });//closed document
--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -2,7 +2,7 @@ var galleryItems = [
     
     {
     id: 0,
-    imgSrc: 'images/dashboard.png',
+    imgSrc: 'assets/images/dashboard.png',
     imgAlt: 'Image of the Web App Dashboard  project',
     heading: 'Web App Dashboard',
     introduction: 'Web App Dashboard is a web dashboard that uses Chart.js to create line, bar, and pie charts and also  uses local storage to save user changes.',
@@ -12,7 +12,7 @@ var galleryItems = [
   },
   {
     id: 1,
-    imgSrc: 'images/video player.png',
+    imgSrc: 'assets/images/video player.png',
     imgAlt: 'Image of the Interactive Video Player project',
     heading:'Interactive Video Player',
     introduction: 'Interactive Video Player is a  custom video player that uses JavaScript to highlight the transcript below the video as the video progresses.Also when the user clicks any part of the transcript it takes them to the appropriate place in the video.',
@@ -21,7 +21,7 @@ var galleryItems = [
   },
   {
     id: 2,
-    imgSrc: 'images/employee.jpg',
+    imgSrc: 'assets/images/employee.jpg',
     imgAlt: 'Image of Employee Directory',
     heading: 'Employee Directory',
     introduction:'Employee Directory uses Random user generator API to generate random users and their details. Clicking the user would popup a modal with more information.This project included Ajax calls using jQuery and CSS. ',
@@ -30,7 +30,7 @@ var galleryItems = [
   },
   {
     id: 3,
-    imgSrc: 'images/inter.png',
+    imgSrc: 'assets/images/inter.png',
     imgAlt: 'Image of the Interactive form project',
     heading: 'Interactive Form',
     introduction: 'Interactive Form uses JavaScript to enhance the form by adding interactivity like providing helpful error messages when the user tries to submit the form and enters invalid information into the form fields.',
@@ -39,7 +39,7 @@ var galleryItems = [
   },
   {
     id: 4,
-    imgSrc: 'images/wheel of sucess.jpg',
+    imgSrc: 'assets/images/wheel of sucess.jpg',
     imgAlt: 'Image of the Wheel of Success project',
     heading: 'Wheel of Success.',
     introduction: 'Wheel Of Sucess is a browser version of a word guessing game. It uses Javascript to generate a random phrase that player tries to guess by clicking the letters from an onscreen keyboard.',
@@ -48,7 +48,7 @@ var galleryItems = [
   },
   {
     id: 5,
-    imgSrc: 'images/photo gallery.png',
+    imgSrc: 'assets/images/photo gallery.png',
     imgAlt: 'Image of the Photo Gallery project',
     heading: 'Photo Gallery',
     introduction: 'Photo Gallery is an interactive photo gallery built using JavaScript and a jQuery plugin for a lightbox. It also has a search box funtionality that filters the photos in real-time as the user types, and only display photos that match the search.',
@@ -57,7 +57,7 @@ var galleryItems = [
   },
   {
     id: 6,
-    imgSrc: 'images/quote.png',
+    imgSrc: 'assets/images/quote.png',
     imgAlt: 'Image of the Quote Generator project',
     heading:'Quote Generator',
     introduction: 'Quote Generator is website built using HTML5, CSS3 and Javascript which shows new quotes and also allows the user to tweet out the quote.',
@@ -66,7 +66,7 @@ var galleryItems = [
   },
   {
     id: 7,
-    imgSrc: 'images/svg.png',
+    imgSrc: 'assets/images/svg.png',
     imgAlt: 'Image of the Svg Animation project',
     heading: 'SVG Animation',
     introduction: 'SVG Animation uses SVGs to spruce up and add a bit of character to a website.The SVGs were modified by manipulating their styles and animating them.',
@@ -75,7 +75,7 @@ var galleryItems = [
   },
   {
     id: 8,
-    imgSrc: 'images/responsive.png',
+    imgSrc: 'assets/images/responsive.png',
     imgAlt: 'Image of the Responsive Website project',
     heading: 'Responsive Layout',
     introduction:'Responsive Layout is website built using mobile-first principals and uses CSS3 Grid for layouts.The purpose of this project was to understand basics of building responsive webpages using HTML5 and CSS3. ',
@@ -84,7 +84,7 @@ var galleryItems = [
   },
   {
     id: 9,
-    imgSrc: 'images/profilepage.png',
+    imgSrc: 'assets/images/profilepage.png',
     imgAlt: 'Image of the Profile Page project',
     heading:'Profile Page',
     introduction: 'Profile Page is one of the first website that I built using HTML5 and CSS3.It uses Responsive Design principals, CSS transforms and CSS3 pseudo elements',
@@ -93,7 +93,7 @@ var galleryItems = [
   },
   {
     id: 10,
-    imgSrc: 'images/styleguide.png',
+    imgSrc: 'assets/images/styleguide.png',
     imgAlt: 'Image of the Web Style Guide project',
     heading:'Web Style Guide',
     introduction: 'Web Style Guide is a style guide created using SASS fundamentals like variables, mixins, and functions.',
@@ -102,7 +102,7 @@ var galleryItems = [
   },
   {
     id: 11,
-    imgSrc: 'images/registration-form.png',
+    imgSrc: 'assets/images/registration-form.png',
     imgAlt: 'Image of the Registration Form project',
     heading: 'Registration Form',
     introduction: 'Registration Form is a mobile first, responsive form built using a variety of HTML form input types and attributes. The form was enhanced using HTML5 validation properties, CSS3 transitions and pseudo-states.',
